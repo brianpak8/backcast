e7c2007a903755581d7c47e5f05e9020be7ae02a
Hack Reactor Student <student@hackreactor.com>
2017-10-23 20:21:01 -0700

    first commit

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,19 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
+   
+    console.log('!!!!!!!!!!!!!!', this.videos);
+    this.videoList = new VideoListView({videos: this.videos});
+    this.videoList.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    //do something here!!!!
+    new VideoListView({videos: this.videos});
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,7 +1,13 @@
 var VideoListView = Backbone.View.extend({
 
-
+  // initialize: function() {
+  //   this.collection.on('sort', this.render, this);
+  // },
+  
   render: function() {
+    for (var i = 0; i < videos.length; i++) {
+      new VideoListEntryView({video: collection[i]});
+    }
     this.$el.children().detach();
     this.$el.html(this.template());
     return this;
